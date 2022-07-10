43784c560a59ccc4eca92038e7dc86855806c58f
wardgeronimussmets <81919921+wardgeronimussmets@users.noreply.github.com>
2022-07-10 18:02:22 +0200

    Ik denk dat het terug werkt

--- model_trainer.py
+++ model_trainer.py
@@ -22,8 +22,8 @@ class OopsEnv(Env):
     def __init__(self):
         # Actions we can take: from 25 positions to 25 other positions -> 5 bit + 5 bit = 10bit
         self.action_space = MultiDiscrete([25, 25])
-        # Observations is gewoon het speelbord
-        self.observation_space = MultiDiscrete(np.array([[21, 21, 21, 21, 21], [21, 21, 21, 21, 21], [21, 21, 21, 21, 21], [21, 21, 21, 21, 21], [21, 21, 21, 21, 21]]))
+        # Observations is gewoon het speelbord, we have to flatten the matrix because otherwise it crashes
+        self.observation_space = MultiDiscrete(np.array([31, 31, 31, 31, 31,31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31, 31]))
         # get the grid from a random level from the level directory
         # state = grid
         self.state = get_grid()
@@ -46,22 +46,22 @@ class OopsEnv(Env):
         end_position = action[1]
         ex, ey = pos2indices(end_position)
         # print("Registered input, from ", bx , by, " to ", ex, ey)
-        if not valid_pickup(bx, by, self.state):
+        if not valid_pickup(begin_position, self.state):
             reward = -20
             # print("Not valid pickup")
             return self.state, reward, done, info
-        elif not valid_end(ex, ey, self.state):
+        elif not valid_end(end_position, self.state):
             reward = -15
             # print("Not valid end")
             return self.state, reward, done, info
-        elif not valid_steps(bx, by, ex, ey, self.state):
+        elif not valid_steps(bx, by, ex, ey,begin_position, self.state):
             reward = -1
             # print("Not valid amount of steps")
             return self.state, reward, done, info
         # print("Valid action")
         # else, the action was valid, could still not be the best one to complete the game ofcourse
         reward = 20
-        self.state = execute_step(bx, by, ex, ey, self.state)
+        self.state = execute_step(begin_position,end_position, self.state)
         if is_game_won(self.state):
             reward = 200
             done = True
@@ -70,6 +70,7 @@ class OopsEnv(Env):
         else:
             # one step done a few more to go
             return self.state, reward, done, info
+        return self.state,0,done,info
 
     def render(self):
         print("#" * 30)
@@ -92,40 +93,40 @@ class OopsEnv(Env):
         return self.state
 
 
-def execute_step(bx, by, ex, ey, grid):
+def execute_step(begin_pos,end_pos,grid):
     # check which piece is going to go where and create the new grid
     # add the two new spaces up
-    grid[ey][ex] += grid[by][bx]
-    # where the piece used to be there will now be an empty space
-    grid[by][bx] = 0
-    return np.array(grid)
+    grid[end_pos] += grid[begin_pos]
+    # # where the piece used to be there will now be an empty space
+    # grid[begin_pos] = 0
+    return grid
 
 
-def valid_pickup(x, y, grid):
+def valid_pickup(begin_pos, grid):
     # target_pos is the position we are trying to pick up, this needs to be valid
     # can't pick up empty space or the hat
-    target_piece = grid[y][x]
+    target_piece = grid[begin_pos]
     if target_piece in [0, 10]:
         return False
     else:
         return True
 
 
-def valid_end(x, y, grid):
+def valid_end(end_pos, grid):
     # we need to put the pieces down on a valid space(not on the wizards' head) and on another piece
-    target_piece = grid[y][x]
+    target_piece = grid[end_pos]
     if target_piece == 0 or target_piece >= 20:
         return False
     else:
         return True
 
 
-def valid_steps(bx, by, ex, ey, grid):
+def valid_steps(bx, by, ex, ey,begin_pos, grid):
     # and we need to check if the amount of steps we have done is valid
     x_dist = abs(bx - ex)
     y_dist = abs(by - ey)
     tot_dist = x_dist + y_dist
-    picked_up_piece = grid[by][bx]
+    picked_up_piece = grid[begin_pos]
     # we need to remove the wizards identifier from the equation ( is 20)
     if picked_up_piece >= 20:
         picked_up_piece -= 19
@@ -139,11 +140,9 @@ def is_game_won(grid):
     # if the game is won we can give the ai a big bonus
     amountOfPiecesFound = 0
     for x in range(len(grid)):
-        for y in range(len(grid[0])):
-            if grid[x][y] != 0:
-                amountOfPiecesFound += 1
+        if grid[x] != 0:
+            amountOfPiecesFound += 1
     if amountOfPiecesFound <= 1:
-        gameFinished = True
         return True
     else:
         return False
@@ -168,7 +167,7 @@ def get_grid():
     selected_level = file_names[select_nr]
     with open(os.path.join(selected_level), "rb") as f:
         grid = pickle.load(f)
-        return np.array(grid)
+        return np.array(grid).flatten()
 
 
 def test_environment_manuel(env):
@@ -196,7 +195,7 @@ def get_env():
 
 def train_model(model, amount_of_steps):
     model.learn(total_timesteps=amount_of_steps)
-    model.save('PPO')
+    model.save(os.path.join("Models","Oops_Model"))
 
 
 def get_model(env):
@@ -205,31 +204,10 @@ def get_model(env):
     return model
 
 
-# # MAIN
-# g_env = get_env()
-# g_env.reset()
-# # test_environment_manuel(g_env)
-# g_model = get_model(g_env)
-# #train_model(g_model, 20000)
+# MAIN
+g_env = get_env()
+g_env.reset()
+# test_environment_manuel(g_env)
+g_model = get_model(g_env)
+train_model(g_model, 100)
 
-env=OopsEnv()
-
-env.reset()
-
-episodes = 5
-for episode in range(1, episodes + 1):
-    state = env.reset()
-    done = False
-    score = 0
-
-    while not done:
-        env.render()
-        action = env.action_space.sample()
-        n_state, reward, done, info = env.step(action)
-        score += reward
-    print('Episode:{} Score:{}'.format(episode, score))
-env.close()
-log_path = os.path.join('Training', 'Logs')
-model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=log_path)
-model.learn(total_timesteps=4000)
-model.save('test')
